ce66d56b1eb820cd7a28f2bbbacd04ced2e7841a
ipz21 <ipz21@KYCHORT>
2023-02-11 12:07:11 +0200

    Add project files.

---
+++ Lab 9.2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_9._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ulong result;
+            int lightSpeed = 299792;/* km/c */
+            int hour = 60 * 60;
+            int day = hour * 24;
+
+            result = (ulong)lightSpeed * (ulong) hour;
+            Console.WriteLine("Hour: " + result + " km.");
+            result = (ulong)lightSpeed * (ulong)day;
+            Console.WriteLine("Day: " + result + " km.");
+            Console.ReadKey();
+        }
+    }
+}
